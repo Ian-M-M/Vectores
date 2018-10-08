a3e70a20cbc47ed3bf08dbfebfb9e7b39eb33e7d
Ian Marrero <32128820+Ian-M-M@users.noreply.github.com>
2018-10-08 21:47:26 +0100

    Add files via upload

--- Vectores/src/vectores.adb
+++ Vectores/src/vectores.adb
@@ -1,5 +1,5 @@
 package body vectores with SPARK_Mode => On is
-   
+
 --     function uppercase (Str : in String) return String is
 --        ascii : Integer;
 --        res : String := Str;
@@ -12,31 +12,34 @@ package body vectores with SPARK_Mode => On is
 --              res(I) := Character'Val(ascii - 32);
 --           end if;
 --        end loop;
---  
+--
 --        return res;
 --     end uppercase;
-   
+
    function suma (vec1 : in Vector; vec2 : in Vector) return Vector is
       res : Vector := vec1;
    begin
-      for I in res'First .. res'Length loop
-         pragma Loop_Invariant(vec1(I) < Integer'Last - vec2(I)); 
+      for I in vec1'Range loop
          res(I) := vec1(I) + vec2(I);
+         pragma Loop_Invariant (for all K in res'First .. I =>
+                                --(vec1(K) <= Integer'Last - vec2(K))
+                                --and (vec1(K) >= Integer'First - vec2(K))
+                                --(res(K) <= Integer'Last)
+                                --and (res(K) >= Integer'First)
+                                (res(K) = vec1(K) + vec2(K)));
       end loop;
-      
+
       return res;
    end suma;
-   
+
 --     function resta (vec1 : in Vector; vec2 : in Vector) return Vector is
 --        res : Vector := vec1;
 --     begin
 --        for I in 1 .. vec1'Length loop
 --           res(I) := vec1(I) - vec2(I);
 --        end loop;
---        
+--
 --        return res;
 --     end resta;
-   
-   
-   
+
 end vectores;
--- Vectores/src/vectores.ads
+++ Vectores/src/vectores.ads
@@ -1,22 +1,32 @@
 
 package vectores with SPARK_Mode => on is
-   --Max : constant := 100;
-   --subtype Index_Type  is Natural range 0 .. Max;
-   type Vector is array(Integer range 1..3) of Integer;
    
-   -- La funcion uppercase devuelve la String pasada por parámetro pero convertida
+   type Vector is array(Positive range 1 .. 10) of Integer;
+
+   -- La funcion uppercase devuelve la String pasada por parametro pero convertida
    --a uppercase.
    --function uppercase (Str : in String) return String;
-   
-   -- La funcion suma hace la suma de 2 vectores y devuelve el restultado en 
+
+   -- La funcion suma hace la suma de 2 vectores y devuelve el restultado en
    --un nuevo vector.
    function suma (vec1 : in Vector; vec2 : in Vector) return Vector
    --Global
    --Depend
-     with Pre => (if vec1'Length = vec2'Length and vec1'Length > 0 then True );
-   
-   -- La funcion resta hace la resta de 2 vectores y devuelve el restultado en 
+     with 
+       Pre => vec1'First = vec2'First 
+       and vec1'Last = vec2'Last 
+       and vec1'Length > 0 
+       and (for all I in vec1'Range =>              
+              (if vec1(I) > 0 and vec2(I) > 0 then
+                   vec1(I)  <= Integer'Last - vec2(I))                
+            and (if vec1(I) < 0 and vec2(I) < 0 then                   
+                   vec1(I) >= Integer'First - vec2(I))),
+     
+       Post => (for all I in vec1'Range => 
+                  suma'Result (I) = vec1(I)+vec2(I));
+       
+   -- La funcion resta hace la resta de 2 vectores y devuelve el restultado en
    --un nuevo vector.
    --function resta (vec1 : in Vector; vec2 : in Vector) return Vector;
-   
+
 end vectores;
